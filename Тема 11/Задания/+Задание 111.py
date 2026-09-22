a819532d994e559b696322c40b578bdc1010723b
oleg <oleg04112009@gmail.com>
2026-09-22 21:45:41 +0300

    Обновлен статус задания № 111 Тема: 11 > Верно

---
+++ Тема 11/Задания/+Задание 111.py
@@ -0,0 +1,15 @@
+# Решение
+
+
+
+
+
+
+
+answer = 24
+
+#
+
+from tests.conftest import result_register
+if answer is not Ellipsis:
+    print(result_register(11, 111, answer, '1ff1de774005f8da13f42943881c655f'))
